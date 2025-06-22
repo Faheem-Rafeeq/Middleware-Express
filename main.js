c4e6007757258f887f5a7b0105d9b2bfbd1b87ab
Faheem-Rafeeq <imfaheemrafeeq78@gmail.com>
2025-06-22 16:25:16 +0500

    middleware

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs')
 
-app.use(express.static("public"))
+// app.use(express.static("public"))
+
+
+
+
+app.use((req, res, next)=> {
+ fs.appendFileSync("logs.txt" , `${Date.now()} is a ${req.method}/n`)
+ console.log(`${Date.now()} is a ${req.method}`)
+  next()
+})
+
+app.use((req, res, next)=> {
+  console.log('LOGGED')
+  next()
+})
+
 
 app.get('/', (req, res) => {
   res.send('Hello World2!')
